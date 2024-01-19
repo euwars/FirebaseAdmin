bf49ebaf4cbd50d07298c5662a8432cfb02decac
Farzad <nazifi10@gmail.com>
2024-01-19 12:35:59 +0400

    Update DocumentReference.swift

--- Sources/Firestore/DocumentReference.swift
+++ Sources/Firestore/DocumentReference.swift
@@ -1,6 +1,6 @@
 //
 //  DocumentReference+gRPC.swift
-//  
+//
 //
 //  Created by Norikazu Muramoto on 2023/04/10.
 //
@@ -9,36 +9,36 @@ import Foundation
 import NIOHPACK
 
 extension DocumentReference {
-
-    public func getDocument() async throws -> DocumentSnapshot {
-        let firestore = Firestore.firestore()
+    
+    public func getDocument(prevFirestore: Firestore? = nil) async throws -> DocumentSnapshot {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
         let headers = HPACKHeaders([("authorization", "Bearer \(accessToken)")])
         return try await getDocument(firestore: firestore, headers: headers)
     }
-
-    public func setData(_ documentData: [String: Any], merge: Bool = false) async throws {
-        let firestore = Firestore.firestore()
+    
+    public func setData(_ documentData: [String: Any], merge: Bool = false, prevFirestore: Firestore? = nil) async throws {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
         let headers = HPACKHeaders([("authorization", "Bearer \(accessToken)")])
         return try await setData(documentData, merge: merge, firestore: firestore, headers: headers)
     }
-
-    public func updateData(_ fields: [String: Any]) async throws {
-        let firestore = Firestore.firestore()
+    
+    public func updateData(_ fields: [String: Any], prevFirestore: Firestore? = nil) async throws {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
         let headers = HPACKHeaders([("authorization", "Bearer \(accessToken)")])
         return try await updateData(fields, firestore: firestore, headers: headers)
     }
-
-    public func delete() async throws {
-        let firestore = Firestore.firestore()
+    
+    public func delete(prevFirestore: Firestore? = nil) async throws {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
@@ -48,18 +48,18 @@ extension DocumentReference {
 }
 
 extension DocumentReference {
-
-    public func setData<T: Encodable>(_ data: T, merge: Bool = false) async throws {
-        let firestore = Firestore.firestore()
+    
+    public func setData<T: Encodable>(_ data: T, merge: Bool = false, prevFirestore: Firestore? = nil) async throws {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
         let headers = HPACKHeaders([("authorization", "Bearer \(accessToken)")])
         return try await self.setData(data, firestore: firestore, headers: headers)
     }
-
-    public func updateData<T: Encodable>(_ data: T) async throws {
-        let firestore = Firestore.firestore()
+    
+    public func updateData<T: Encodable>(_ data: T, prevFirestore: Firestore? = nil) async throws {
+        let firestore = prevFirestore ?? Firestore.firestore()
         guard let accessToken = try await firestore.getAccessToken() else {
             fatalError("AcessToken is empty")
         }
